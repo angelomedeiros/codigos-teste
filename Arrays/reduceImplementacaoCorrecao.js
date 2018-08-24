073b07736e6546b16ce7ae092bcaba6d403928ca
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-24 14:59:16 -0300

    Bugfix: Correcao da implementacao do metodo reducer2

--- Arrays/reduceImplementacaoCorrecao.js
+++ Arrays/reduceImplementacaoCorrecao.js
@@ -1,6 +1,15 @@
 Array.prototype.reduce2 = function(callback, valorIncial) {
-  const indiceInical = valorIncial ? 0 : 1
-  let acumulador = valorIncial || this[0]
+  
+  let indiceInical = 0
+  let acumulador = 0
+
+  if ( valorIncial === 0 || !!valorIncial) {
+    indiceInical = 0
+    acumulador = valorIncial
+  } else {
+    indiceInical = 1
+    acumulador = this[0]
+  }
 
   for (let i = indiceInical; i < this.length ; i++ ) {
     acumulador = callback(acumulador, this[i], i, this)
@@ -15,5 +24,6 @@ const nums = [1, 2, 3, 4, 5]
 
 const resultado = nums.reduce2(mult) // (OK) Espera 120, obteve 120
 const resultado2 = nums.reduce2(mult, 2) // (OK) Espera 240, obteve 240
-const resultado3 = nums.reduce2(mult, 0) // (ERRO) Espera 0, obteve 120
+const resultado3 = nums.reduce2(mult, 0) // (Ok) Espera 0, obteve 0
 console.log(resultado, resultado2, resultado3)
+
